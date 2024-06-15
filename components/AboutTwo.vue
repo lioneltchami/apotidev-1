cf8eeb7bd3b3f82e4209d379ea730d2970a016f4
apotitech <lioneltchami@gmail.com>
2024-06-14 17:04:09 -0700

    feat/aboutus
    Because the About Us page was still using template data. I updated it
    with more up to date information. This is the first step in updating the
    website. The next step after this is working on content folder.

--- components/AboutTwo.vue
+++ components/AboutTwo.vue
@@ -5,7 +5,7 @@
                 <div class="col-lg-9">
                     <div class="section-title">
                         <h5 class="subtitle line-theme-color">About Us.</h5>
-                        <h2 class="title"><span>ADA</span> is The Non Profit Organization.</h2>
+                        <h2 class="title"><span>A.D.A.</span> is The Non Profit Organization.</h2>
                     </div>
                 </div>
             </div>
@@ -17,8 +17,8 @@
                 </div>
                 <div class="col-lg-8">
                     <div class="about-content">
-                        <p class="text-style">Lorem Ipsum simply dummy text the printing and typesettin industry. Lorem Ipsum has been the industry's standard dumm text ever since the unknown.</p>
-                        <p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when and unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p class="text-style">At Apoti Development Association (A.D.A.), we are driven by a deep commitment to extend the love and compassion of Jesus Christ to the more vulnerable members of our society. Our organization was founded on the belief that every individual deserves to live with dignity, hope, and self-sufficiency.</p>
+                        <p class="mb-0">Through our dedicated efforts, we strive to provide essential support in the form of financial assistance, training, food, clothing, and other resources. Our goal is to uplift and empower individuals, fostering a sense of hope and empowerment, and creating a community where everyone has access to the resources they need to thrive.</p>
                     </div>
                     <div class="content-box-wrp">
                         <div class="row mtn-30">
@@ -26,17 +26,17 @@
                                 <div class="content-box-item">
                                     <h3 class="title">Mission</h3>
                                     <img class="img-line-shape" src="/images/shape/line-s1.png" alt="image">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry has been the industry's standard dummy text ever since 1500 when unknown printer took a galley of type and scram bled it to make a type specimen book.</p>
-                                    <p class="font-family2 mb-0">Lorem Ipsum simply dummy text the printing & typesetting industry been the industry standard dummy make type specimen book.</p>
+                                    <p>We uphold values of compassion, integrity, empowerment, and community, offering financial aid, training, food, clothing, and other resources. Our goal is to uplift and empower individuals, fostering hope, dignity, and self-sufficiency. </p>
+                                    <p class="font-family2 mb-0">Our mission is to show Jesus Christ's love and compassion to society's most vulnerable. </p>
                                     <img class="bg-line-shape" src="/images/shape/line5.png" alt="image">
                                 </div>
                             </div>
                             <div class="col-lg-6 mt-30">
                                 <div class="content-box-item" :style="{backgroundColor: '#fea444'}">
-                                    <h3 class="title">Vission</h3>
+                                    <h3 class="title">Vision</h3>
                                     <img class="img-line-shape" src="/images/shape/line-s1.png" alt="image">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry has been the industry's standard dummy text ever since 1500 when unknown printer took a galley of type and scram bled it to make a type specimen book.</p>
-                                    <p class="font-family2 mb-0">Lorem Ipsum simply dummy text the printing & typesetting industry been the industry standard dummy make type specimen book.</p>
+                                    <p> We envision a community where everyone has access to the resources they need to thrive. Guided by our core values, we aim to build a society rooted in compassion, inspiring others to join us, make a lasting, positive impact on the world.</p>
+                                    <p class="font-family2 mb-0">We dream of a world where the vulnerable realize just how much God loves them.</p>
                                     <img class="bg-line-shape" src="/images/shape/line5.png" alt="image">
                                 </div>
                             </div>
